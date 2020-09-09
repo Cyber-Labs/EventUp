beadb1d3bb1499e011b0017c95d666109d82b7d2
Aman-Codes <54680709+Aman-Codes@users.noreply.github.com>
2020-09-09 15:25:02 +0530

    Added send email feature

--- controllers/User/auth.js
+++ controllers/User/auth.js
@@ -1,6 +1,7 @@
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
+const nodemailer = require("nodemailer");
 
 exports.register = (req, res) => {
     const { name, email, password } = req.body;
@@ -42,6 +43,35 @@ exports.register = (req, res) => {
 
 exports.login = (req, res) => {
     const { email, password } = req.body;
+
+    const senderEmail = require('../../config/keys').email;
+    const senderPassword = require('../../config/keys').password;
+    const receiverEmail = ["aman.dwivedi5@gmail.com","help.eventup@gmail.com" ];
+    const emailMessage = "This is a testing email !!!";
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: senderEmail,
+          pass: senderPassword
+        }
+      });
+  
+    var mailOptions = {
+        from: senderEmail,
+        to: receiverEmail,
+        subject: 'Test Email',
+        text: emailMessage,
+    };
+  
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent successfully' );
+        }
+    });   
+
     // check if user exist
     User.findOne({ email }).exec((err, user) => {
         if (err || !user) {
